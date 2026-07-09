33dc2d3970ae07f77010c9038ccb5a11bab37ae0
Habiba Ayman <145665685+habibaaymannn@users.noreply.github.com>
2026-07-09 17:30:08 +0300

    initial commit

---
+++ src/main/java/com/example/course_manag_system/CourseManagSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_manag_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseManagSystemApplication.class, args);
+    }
+
+}
